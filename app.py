0f5c937b0278aca1b8d9b6cb9db74fe8aff35215
Anthony Macias <Anthony40macias@gmail.com>
2023-12-23 22:58:35 -0800

    full text reader without filter on PDF files.

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 import streamlit as st 
 from dotenv import load_dotenv
+from PyPDF2 import PdfReader
 
+def pdf_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            page_text = page.extract_text()
+            if page_text:  # Check if text is extracted
+                text += page_text
+    return text
+
+def get_text_chucks(raw_text):
 
 def main():
     load_dotenv()
@@ -10,9 +22,14 @@ def main():
     
     with st.sidebar:
         st.subheader("Your Documents")
-        st.file_uploader("Upload your PDF documents here and click on 'Processing' ")
-        st.button("Processing")
+        pdf_docs = st.file_uploader("Upload your PDF documents here and click on 'Processing'", accept_multiple_files=True)
+        if st.button("Processing") and pdf_docs:
+            with st.spinner("Processing your documents..."):
+                raw_text = pdf_pdf_text(pdf_docs)
+                st.write(raw_text)
+                #get chuck of text
+                text_chucks = get_text_chucks(raw_text)
+#               #create vector store
 
 if __name__ == '__main__':
     main()
-
